25622742e456773820c547b41e791fa904b348cc
Mosolasi Akinyemi <akinyemimosolasi@gmail.com>
2021-04-19 13:29:24 +0100

    chore: setup project

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const App = () => {
+  return <View style={styles.backgroundStyle}></View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
---
+++ android/app/src/main/java/com/mishael_native/MainActivity.java
@@ -0,0 +1,27 @@
+package com.mishael_native;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import java.util.Map;
+import java.util.HashMap;
+
+public class CalendarModule extends ReactContextBaseJavaModule {
+  CalendarModule(ReactApplicationContext context) {
+    super(context);
+  }
+}
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "mishael_native";
+  }
+}
